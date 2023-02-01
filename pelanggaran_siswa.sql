42513881c57665ed3aa14c3e072dcfe4061090c2
aureliiiia <100220192+aureliiiia@users.noreply.github.com>
2023-02-01 12:08:54 +0700

    Add files via upload

---
+++ pelanggaran_siswa.sql
@@ -0,0 +1,201 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 01, 2023 at 06:08 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pelanggaran_siswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_pelanggaran_siswa`
+--
+
+CREATE TABLE `detail_pelanggaran_siswa` (
+  `id_pelanggaran_siswa` int(11) NOT NULL,
+  `id_pelanggaran` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `detail_pelanggaran_siswa`
+--
+
+INSERT INTO `detail_pelanggaran_siswa` (`id_pelanggaran_siswa`, `id_pelanggaran`) VALUES
+(0, 1),
+(0, 2),
+(0, 1),
+(0, 2),
+(0, 1),
+(0, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggaran`
+--
+
+CREATE TABLE `pelanggaran` (
+  `id_pelanggaran` int(11) NOT NULL,
+  `nama_pelanggaran` varchar(300) NOT NULL,
+  `poin` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pelanggaran`
+--
+
+INSERT INTO `pelanggaran` (`id_pelanggaran`, `nama_pelanggaran`, `poin`) VALUES
+(1, 'terlambat', 10),
+(2, 'atribut tidak lengkap', 15),
+(3, 'berkelahi', 15),
+(4, 'mencuri', 25),
+(5, 'merusak fasilitas', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggaran_siswa`
+--
+
+CREATE TABLE `pelanggaran_siswa` (
+  `id_pelanggaran_siswa` int(11) NOT NULL,
+  `waktu` datetime NOT NULL,
+  `id_siswa` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pelanggaran_siswa`
+--
+
+INSERT INTO `pelanggaran_siswa` (`id_pelanggaran_siswa`, `waktu`, `id_siswa`, `id_user`) VALUES
+(17, '2023-02-01 11:23:12', 3, 1),
+(18, '2023-02-01 11:34:48', 3, 1),
+(19, '2023-02-01 11:40:11', 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `id_siswa` int(11) NOT NULL,
+  `nis` varchar(20) NOT NULL,
+  `nama_siswa` varchar(300) NOT NULL,
+  `kelas` varchar(20) NOT NULL,
+  `poin` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `siswa`
+--
+
+INSERT INTO `siswa` (`id_siswa`, `nis`, `nama_siswa`, `kelas`, `poin`) VALUES
+(3, '198', 'jhon', 'X RPL 10', 100),
+(5, '130', 'manda', 'XI RPL 8', 50),
+(6, '122', 'marsha', 'XI RPL 8', 150),
+(7, '127', 'yeyen', 'XI RPL 5', 150),
+(8, '131', 'pandu', 'XI RPL 7', 50);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `nama_user` varchar(300) NOT NULL,
+  `username` varchar(300) NOT NULL,
+  `password` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `nama_user`, `username`, `password`) VALUES
+(1, 'bu ratih', 'bu ratt', '123'),
+(3, 'bu aulia', 'bu aul', '234'),
+(4, 'pak thoriq', 'pak t', '345'),
+(5, 'pak firman', 'pak fir', '456');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pelanggaran`
+--
+ALTER TABLE `pelanggaran`
+  ADD PRIMARY KEY (`id_pelanggaran`);
+
+--
+-- Indexes for table `pelanggaran_siswa`
+--
+ALTER TABLE `pelanggaran_siswa`
+  ADD PRIMARY KEY (`id_pelanggaran_siswa`),
+  ADD KEY `id_siswa` (`id_siswa`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indexes for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`id_siswa`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pelanggaran`
+--
+ALTER TABLE `pelanggaran`
+  MODIFY `id_pelanggaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `pelanggaran_siswa`
+--
+ALTER TABLE `pelanggaran_siswa`
+  MODIFY `id_pelanggaran_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `siswa`
+--
+ALTER TABLE `siswa`
+  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
